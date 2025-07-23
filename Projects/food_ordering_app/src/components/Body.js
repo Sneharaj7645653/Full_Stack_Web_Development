6620b9f32a1686be7d61633f5a2ade11e41b12a0
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-07-23 17:31:44 +0530

    react hook useState added

--- Projects/food_ordering_app/src/components/Body.js
+++ Projects/food_ordering_app/src/components/Body.js
@@ -1,10 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
 import objList from "../utils/mockData";
+import { useState } from "react";
+
+
 
 const Body = () => {
+    let [restaurantList , setRestaurantList] = useState([data]);
     return (
         <div className="body">
             <div className="search">🔍  Search </div>
+            <div className="filter">
+                <button className="filter-btn" onClick={()=>{
+                    const filteredList = restaurantList.filter((restaurant) => restaurant.info.avgRating > 4.0);
+                    setRestaurantList(filteredList);
+                    console.log("Top Rated Restaurants Filter Applied");
+                    
+                }}>Top Rated Restaurants</button>
+            </div>
             <div className="res-container">
                 {
                     objList.map((restaurant) => {
