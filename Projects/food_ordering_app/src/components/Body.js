6989d2774b4fbcea74c467a4fc93a13ca9e6bd5c
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-07-23 22:39:35 +0530

    actual api used

--- Projects/food_ordering_app/src/components/Body.js
+++ Projects/food_ordering_app/src/components/Body.js
@@ -1,11 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
 import objList from "../utils/mockData";
-import { useState } from "react";
+import { use, useState } from "react";
+import { useEffect } from "react";
+import Shimmer from "./Shimmer";
+
 
 
 
 const Body = () => {
-    let [restaurantList , setRestaurantList] = useState(objList);
+    let [restaurantList , setRestaurantList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+const fetchData = async () => {
+    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const jsonData = await data.json();
+    console.log(jsonData.data.cards[0]);
+    setRestaurantList(jsonData);
+
+};
+
+
+if(restaurantList.length === 0) {
+    return <Shimmer />; 
+}
+
     return (
         <div className="body">
             <div className="search">🔍  Search </div>
