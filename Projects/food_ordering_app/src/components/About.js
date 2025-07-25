e28a280daa003aee0c4585da349489c8e7ceb473
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-07-25 21:43:13 +0530

    class component: Users

--- Projects/food_ordering_app/src/components/About.js
+++ Projects/food_ordering_app/src/components/About.js
@@ -1,9 +1,8 @@
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
-        <div>
-            <h1>About Page</h1>
-            <h2>You reached by routing to "/about"!!</h2>
-        </div>
+        <UserClass />
         
     );
 };
---
+++ Projects/food_ordering_app/src/components/UserClass.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class UserClass extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>NAME Component</h1>
+            <h2>This is a class component for user details.</h2>
+            <p>User ID: </p>
+            <p>User Name: </p>
+        </div>
+    );
+    }
+};
+
+export default UserClass;
