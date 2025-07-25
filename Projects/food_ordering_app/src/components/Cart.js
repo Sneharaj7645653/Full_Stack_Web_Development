cd1b01493c7a5b46b1f4e1038274fd9afb26530c
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-07-25 13:47:43 +0530

    dummy Cart.js added

---
+++ Projects/food_ordering_app/src/components/Cart.js
@@ -0,0 +1,11 @@
+const Cart = () => {
+    return (
+        <div>
+            <h1>Cart Page</h1>
+            <h2>You reached by routing to "/cart"!!</h2>
+        </div>
+        
+    );
+};
+
+export default Cart;
