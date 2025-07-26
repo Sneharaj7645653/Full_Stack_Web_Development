f0cffeff5d3de630632b224e1b562480579afc19
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-07-26 18:01:49 +0530

    lazy loading added

---
+++ Projects/food_ordering_app/src/components/Grocery.js
@@ -0,0 +1,10 @@
+const Grocery = () => {
+    return (
+        <div className="grocery">
+            <h1>Grocery Store</h1>
+            <p>Find all your grocery needs here!</p>
+        </div>
+    );
+};
+
+export default Grocery;
