648ba76316ac359c34c3bb0c6603ebea8601d9d0
Sneharaj7645653 <sneharaj7645653@gmail.com>
2025-07-29 13:38:18 +0530

    contacts test covered

--- Projects/food_ordering_app/src/components/__test__/Contacts.test.js
+++ Projects/food_ordering_app/src/components/__test__/Contacts.test.js
@@ -2,14 +2,25 @@ import { render,screen } from "@testing-library/react";
 import Contacts from "../Contacts";
 import "@testing-library/jest-dom";
 
-test("Should load the contacts component", ()=>{
-    render(<Contacts />);
-    const heading = screen.getByText("📞 Contacts Page");
-    expect(heading).toBeInTheDocument();
-});
 
-test("Should load the contacts component", ()=>{
+describe("contacts page test cases",()=>{
+    test("Should load the contacts component", ()=>{
     render(<Contacts />);
     const heading = screen.getByText("📞 Contacts Page");
     expect(heading).toBeInTheDocument();
+    });
+
+    test("Should load the button component", ()=>{
+        render(<Contacts />);
+        const button = screen.getByRole("button");
+        expect(button).toBeInTheDocument();
+    });
+
+    it("Should load three input boxes", ()=>{
+        render(<Contacts />);
+        const textbox = screen.getAllByRole("textbox");
+        const len = textbox.length;
+        expect(len).toBe(3);
+    })
+
 })
